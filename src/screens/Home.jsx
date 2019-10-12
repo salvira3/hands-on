ae2bc3bc97239fd92fee750941f52a3acaa45589
Cut Syifa Salvira <salviraa3@gmail.com>
2019-10-12 23:51:24 +0700

    setup font and initialize next section

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -10,6 +10,15 @@ export class Home extends React.Component {
     return (
       <div>
         <Header title="Get your real work experience here!" subtitle="Get a chance to try your dream job position. Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
+        <div style={{backgroundColor: "#F4F4F4"}}>
+          <div className="container pt-5">
+            <div className="row">
+              <div className="col-12">
+                <h1 className="bold-title">Available Hands-On Courses</h1>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
