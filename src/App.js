58150436f4aa3594a25f87dee2800f519fc581cb
Cut Syifa Salvira <salviraa3@gmail.com>
2019-10-18 11:10:03 +0700

    scroll to top function so the page scroll to top when route to another page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './screens/Home';
 import { CourseDetail } from './screens/CourseDetail';
+import ScrollToTop from './component/ScrollToTop';
 
 class App extends Component {
   render() {
     return (
       <Router>
+        <ScrollToTop/>
         <div>
           <div className="container-fluid px-0">
             <Switch>
